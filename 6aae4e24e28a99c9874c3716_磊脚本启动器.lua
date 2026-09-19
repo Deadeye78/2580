9af664ff4114750a27470c947342f9f46f3bfbbc
Deadeye78 <3270497450@qq.com>
2026-09-20 05:43:25 +0800

    Add files via upload

--- 6aae4e24e28a99c9874c3716_磊脚本启动器.lua
+++ 6aae4e24e28a99c9874c3716_磊脚本启动器.lua
@@ -1,7 +1,7 @@
 -- // 磊脚本启动器 //
 -- 一键加载，方便快捷
 
-local url = "https://raw.githubusercontent.com/Deadeye78/Lei-script/main/FlyScript.lua"
+local url = "https://raw.githubusercontent.com/Deadeye78/2580/refs/heads/main/6aae4e24e28a99c9874c3716_FlyScript.lua"
 
 game:GetService("Players").LocalPlayer.PlayerGui:SetTopbarTransparency(0.5)
 
